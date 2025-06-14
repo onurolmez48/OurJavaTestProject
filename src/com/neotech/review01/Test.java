9826af5c0e291ed669fe8b56f8d62e3fe20991fc
Onur Olmez <onurolmez123@gmail.com>
2025-06-14 23:36:15 +0300

    User 1 made changes

--- src/com/neotech/review01/Test.java
+++ src/com/neotech/review01/Test.java
@@ -3,11 +3,11 @@ package com.neotech.review01;
 public class Test {
 
 	public void test1() {
-		
+		System.out.println("Test");
 	}
 	
 	public static void test2() {
-		
+		System.out.println("Test");
 	}
 	public static void main(String[] args) {
 		
