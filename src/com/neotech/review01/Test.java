e28020ed5454ef3ede490e8ee9f0724ede3410cc
Onur Olmez <onurolmez123@gmail.com>
2025-06-14 23:13:14 +0300

    Added two test methods

--- src/com/neotech/review01/Test.java
+++ src/com/neotech/review01/Test.java
@@ -2,6 +2,13 @@ package com.neotech.review01;
 
 public class Test {
 
+	public void test1() {
+		
+	}
+	
+	public static void test2() {
+		
+	}
 	public static void main(String[] args) {
 		
 		// change
